62ed3b174284d9f4e3283fe951e001a46b09fdbf
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-10-16 21:05:45 -0400

    webpack boiler plate

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const App = () => {
+  return <React.Fragment>{"Hello world"}</React.Fragment>;
+};
+ReactDOM.render(<App />, document.getElementById("app"));
